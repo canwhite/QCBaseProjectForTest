c1174a2077d481f0f6b1fba21f514e0eaaef45e4
Tony <tony@gmail.com>
2018-09-08 10:08:17 +0800

    BaseProject
    BaseProject

---
+++ QCBaseProjectForTest/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  QCBaseProjectForTest
+//
+//  Created by EricZhang on 2018/9/8.
+//  Copyright © 2018年 BYX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
